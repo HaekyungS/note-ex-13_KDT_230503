e910f6a27186dde0590077d323eb6cfce4d00efb
HaekyungSeong <haekyungs95@gmail.com>
2023-05-07 18:44:42 +0900

    결과값이 나오지 않는다.. 만들다 그냥 헬이 된거같..

--- callbackhell.js
+++ callbackhell.js
@@ -1,21 +1,23 @@
-function exampleOne(data){
-  if(Number.isInteger(data)===true){
-    (data)=>{
-      data+=1;
-      (data)=>{
-        data+=2;
-        (data)=>{
-          data+=3;
-          (data)=>{
-            data+=4;
-            return data
-          }
-        }
-      }
-    }
-  }else{
+function exampleOne(data, callback) {
+  if (Number.isInteger(data) === true) {
+    callback
+  } else {
     return new Error('이 함수는 정수가 필요합니다.')
   }
 }
 
-console.log(exampleOne(10))
+
+console.log(exampleOne(10, () => {
+  data += 1;
+  (data) => {
+    data += 2;
+    (data) => {
+      data += 3;
+      (data) => {
+        data += 4;
+        return data
+      }
+    }
+  }
+}))
+
